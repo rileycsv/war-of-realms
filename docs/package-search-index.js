ec7bb7d28fcf3b95ad6bd7192c88057a19984a18
PeterTracy27 <ptracy@daltonstate.edu>
2026-04-22 15:29:17 -0400

    Removed docs/ folder

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"core"},{"l":"entities"},{"l":"environment"},{"l":"environment.tile"},{"l":"main"},{"l":"ui"},{"l":"utils"}];updateSearchResults();
