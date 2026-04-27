bab03975735bb31df7f7b87ea44254fa5af9f7d8
ABriscoe13 <abriscoe@daltonstate.edu>
2026-04-27 09:40:21 -0400

    Fixed Archer Values again

--- src/entities/Archer.java
+++ src/entities/Archer.java
@@ -6,11 +6,11 @@ public class Archer extends Unit {
 
     public Archer(int PID, int UID, String kingdom, int x, int y) {
         super(PID, UID, kingdom, x, y);
-        super.UNIT_TYPE = "Archers";
-        this.maxMovement = DatabaseIO.getValue(super.UNIT_TYPE, "Movement_Speed");
-        this.health = DatabaseIO.getValue(super.UNIT_TYPE, "UnitHealth");
+        super.UNIT_TYPE = "archer.png";
+        this.maxMovement = DatabaseIO.getValue("Archers", "Movement_Speed");
+        this.health = DatabaseIO.getValue("Archers", "UnitHealth");
         this.attackDamage = 2;
-        this.attackRange = DatabaseIO.getValue(super.UNIT_TYPE, "Attack_Range");
+        this.attackRange = DatabaseIO.getValue("Archers", "Attack_Range");
         this.currentHealth = health;
         this.currentMovement = maxMovement;
     }
