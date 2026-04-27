a4c52e215bb5bce247029607092da8683b3ab874
ABriscoe13 <abriscoe@daltonstate.edu>
2026-04-27 09:09:41 -0400

    Fixed Cavalry Values

--- src/entities/Cavalry.java
+++ src/entities/Cavalry.java
@@ -6,11 +6,11 @@ public class Cavalry extends Unit {
 
     public Cavalry(int PID, int UID, String kingdom, int x, int y) {
         super(PID, UID, kingdom, x, y);
-        super.UNIT_TYPE = "cavalry.png";
-        this.maxMovement = DatabaseIO.getValue(super.UNIT_TYPE, "movement");
-        this.health = DatabaseIO.getValue(super.UNIT_TYPE, "health");
-        this.attackDamage = DatabaseIO.getValue(super.UNIT_TYPE, "attackDamage");
-        this.attackRange = DatabaseIO.getValue(super.UNIT_TYPE, "attackRange");
+        super.UNIT_TYPE = "Cavalry";
+        this.maxMovement = DatabaseIO.getValue(super.UNIT_TYPE, "Movement_Speed");
+        this.health = DatabaseIO.getValue(super.UNIT_TYPE, "UnitHealth");
+        this.attackDamage = 7;
+        this.attackRange = DatabaseIO.getValue(super.UNIT_TYPE, "Attack_Range");
         this.currentHealth = health;
         this.currentMovement = maxMovement;
     }
