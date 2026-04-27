991b3ca3fd5ad464d5b81cbdefdf23c6aa60e14a
ABriscoe13 <abriscoe@daltonstate.edu>
2026-04-27 09:42:09 -0400

    Fixed Infantry Values again

--- src/entities/Infantry.java
+++ src/entities/Infantry.java
@@ -5,11 +5,11 @@ import utils.DatabaseIO;
 public class Infantry extends Unit {
     public Infantry(int PID, int UID, String kingdom, int x, int y) {
         super(PID, UID, kingdom, x, y);
-        super.UNIT_TYPE = "Infantry";
-        this.maxMovement = DatabaseIO.getValue(super.UNIT_TYPE, "Movement_Speed");
-        this.health = DatabaseIO.getValue(super.UNIT_TYPE, "UnitHealth");
+        super.UNIT_TYPE = "infantry.png";
+        this.maxMovement = DatabaseIO.getValue("Infantry", "Movement_Speed");
+        this.health = DatabaseIO.getValue("Infantry", "UnitHealth");
         this.attackDamage = 9;
-        this.attackRange = DatabaseIO.getValue(super.UNIT_TYPE, "Attack_Range");
+        this.attackRange = DatabaseIO.getValue("Infantry", "Attack_Range");
         this.currentHealth = health;
         this.currentMovement = maxMovement;
     }
