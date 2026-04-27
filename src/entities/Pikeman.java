6f718ba05fc947286899eb2f6673088f48ad443f
ABriscoe13 <abriscoe@daltonstate.edu>
2026-04-27 09:42:57 -0400

    Fixed Pikeman Values again

--- src/entities/Pikeman.java
+++ src/entities/Pikeman.java
@@ -6,11 +6,11 @@ public class Pikeman extends Unit {
 
     public Pikeman(int PID, int UID, String kingdom, int x, int y) {
         super(PID, UID, kingdom, x, y);
-        super.UNIT_TYPE = "Pikemen";
-        this.maxMovement = DatabaseIO.getValue(super.UNIT_TYPE, "Movement_Speed");
-        this.health = DatabaseIO.getValue(super.UNIT_TYPE, "UnitHealth");
+        super.UNIT_TYPE = "pikeman.png";
+        this.maxMovement = DatabaseIO.getValue("Pikemen", "Movement_Speed");
+        this.health = DatabaseIO.getValue("Pikemen", "UnitHealth");
         this.attackDamage = 5;
-        this.attackRange = DatabaseIO.getValue(super.UNIT_TYPE, "Attack_Range");
+        this.attackRange = DatabaseIO.getValue("Pikemen", "Attack_Range");
         this.currentHealth = health;
         this.currentMovement = maxMovement;
     }
